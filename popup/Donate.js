79a40913c409f439958c47563f79d94046d659ed
chaueq <chaueq@protonmail.com>
2019-05-18 12:06:05 +0200

    Changed code formating

--- popup/Donate.js
+++ popup/Donate.js
@@ -1,6 +1,7 @@
 function chaueqDonate() {
-    window.open("https://paypal.me/chaueq")
+    window.open("https://paypal.me/chaueq");
 }
 
-const btn = document.getElementById("DonateButton")
-btn.addEventListener("click", chaueqDonate)
+const btn = document.getElementById("DonateButton");
+btn.addEventListener("click", chaueqDonate);
+
--- popup/Hostname.js
+++ popup/Hostname.js
@@ -1,7 +1,7 @@
 function setHostname(tabs) {
-    document.getElementById("Hostname").innerHTML =
-        tabs[0].url.split("/")[0] + "//" + tabs[0].url.split("/")[2]
+	document.getElementById("Hostname").innerHTML = tabs[0].url.split("/")[0] + "//" + tabs[0].url.split("/")[2];
 }
 
-var querying = browser.tabs.query({ currentWindow: true, active: true })
-querying.then(setHostname)
+var querying = browser.tabs.query({ currentWindow: true, active: true });
+querying.then(setHostname);
+
