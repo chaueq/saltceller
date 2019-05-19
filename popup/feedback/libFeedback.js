92f3d1b80c8100f93b3b6fe02af8cd6603819424
chaueq <chaueq@protonmail.com>
2019-05-19 17:21:49 +0200

    Fixed feedback \'undefined\' bug

--- popup/feedback/libFeedback.js
+++ popup/feedback/libFeedback.js
@@ -1,5 +1,5 @@
 function randomEmoji(symbols){
 	if(symbols === undefined)
 		return "😵";
-	return symbols[Math.round(Math.random() * symbols.length)]; 
+	return symbols[Math.round(Math.random() * (symbols.length - 1))]; 
 }
---
+++ popup/feedback/sthWentWrong.js
@@ -0,0 +1,6 @@
+document.getElementById('hugeEmoji').innerHTML = randomEmoji([
+	'😔',
+	'😵',
+	'😕',
+	'❌'
+]);
